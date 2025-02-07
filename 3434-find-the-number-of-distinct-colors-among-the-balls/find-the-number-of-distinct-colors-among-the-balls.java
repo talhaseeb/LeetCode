8ef779e695b6faa9a601f38bdaa1a88fb0599a70
talhaseeb <52654397+talhaseeb@users.noreply.github.com>
2025-02-06 20:54:10 -0500

    Time: 41 ms (60.70%) | Memory: 81.8 MB (84.72%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -3,7 +3,6 @@ class Solution {
       int n = queries.length;
         int[] result = new int[n];
         
-        // Maps to keep track of ball colors and frequencies of colors.
         Map<Integer, Integer> ballColor = new HashMap<>();
         Map<Integer, Integer> colorFreq = new HashMap<>();
         
@@ -14,7 +13,6 @@ class Solution {
             if (ballColor.containsKey(ball)) {
                 int oldColor = ballColor.get(ball);
                 if (oldColor != newColor) {
-                    // Decrease frequency of the old color.
                     int freq = colorFreq.get(oldColor);
                     if (freq == 1) {
                         colorFreq.remove(oldColor);
@@ -22,19 +20,15 @@ class Solution {
                         colorFreq.put(oldColor, freq - 1);
                     }
                     
-                    // Update ball's color to newColor.
                     ballColor.put(ball, newColor);
                     
-                    // Increase frequency of newColor.
                     colorFreq.put(newColor, colorFreq.getOrDefault(newColor, 0) + 1);
                 }
             } else {
-                // Ball wasn't colored before.
                 ballColor.put(ball, newColor);
                 colorFreq.put(newColor, colorFreq.getOrDefault(newColor, 0) + 1);
             }
             
-            // The number of distinct colors is the size of colorFreq.
             result[i] = colorFreq.size();
         }
         
