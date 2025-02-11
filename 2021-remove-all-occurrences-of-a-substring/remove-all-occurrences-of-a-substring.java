5ed433555566930563da44d06635139964994850
talhaseeb <52654397+talhaseeb@users.noreply.github.com>
2025-02-10 20:25:31 -0500

    Time: 4 ms (49.85%) | Memory: 45.1 MB (12.40%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder();
+        int partLen = part.length();
+        
+        for (char c : s.toCharArray()) {
+            sb.append(c);
+            if (sb.length() >= partLen && sb.substring(sb.length() - partLen).equals(part)) {
+                sb.delete(sb.length() - partLen, sb.length());
+            }
+        }
+        
+        return sb.toString();
+    }
+}
